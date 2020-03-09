30c2197244d156b7888621f1c3894c65039738ab
eunmi-baek <eunmi.dev@gmail.com>
2020-03-09 11:53:07 +0900

    초기 세팅

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "Routes";
+import "Styles/index.scss";
+import "Styles/Reset.scss";
+import "Styles/Common.scss";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
