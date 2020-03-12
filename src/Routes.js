15fca646e1446097e5d886e14e226e3b8a2f51e7
kwonki <kihyeon8949@gmail.com>
2020-03-12 15:55:50 +0900

    WIP

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "Pages/Main/Main";
+// import Main from "Pages/Main/Main";
+import CheckIn from "Pages/CheckIn/CheckIn.js";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>{/* <Route exact path="/" component={Main} /> */}</Switch>
+        <Switch>
+          <Route exact path="/" component={CheckIn} />
+        </Switch>
       </Router>
     );
   }
