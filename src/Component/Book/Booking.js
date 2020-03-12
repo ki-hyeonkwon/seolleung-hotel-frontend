d3278150007bcebd2b3d5ddf09f1b0f87e6b88c3
kwonki <kihyeon8949@gmail.com>
2020-03-12 10:35:22 +0900

    nav바 레이아웃/ find hotel 레이아웃 진행 중

---
+++ src/Component/Book/Booking.js
@@ -0,0 +1,66 @@
+import React, { Component, useState } from "react";
+import styled, { css, keyframes } from "styled-components";
+import BookList from "Component/Book/BookList.js";
+
+const Booking = () => {
+  return (
+    <BookingAreaWrapper>
+      <BookingArea>
+        <BookingOn>
+          <BookList
+            listTitle="ROOM"
+            firstList="DELUXE"
+            secondList="FAMILY"
+            thirdList="DELUXE SUITE"
+            forthList="CORNER SUITE"
+            fifthList="PREMIUM SUITE"
+            sixthList="FAMILY SUITE"
+          />
+          <BookList
+            fix="Adult"
+            listTitle=""
+            firstList="1"
+            secondList="2"
+            thirdList="3"
+            forthList="4"
+            fifthList="5"
+            sixthList="6"
+          />
+          <BookList
+            fix="Chidren"
+            listTitle=""
+            firstList="1"
+            secondList="2"
+            thirdList="3"
+            forthList="4"
+            fifthList="5"
+            sixthList="6"
+          />
+        </BookingOn>
+      </BookingArea>
+    </BookingAreaWrapper>
+  );
+};
+export default Booking;
+
+const BookingAreaWrapper = styled.div`
+  position: absolute;
+  bottom: 25%;
+  left: 10%;
+`;
+const BookingArea = styled.div`
+  width: 482px;
+  height: 372px;
+  border-bottom: 3px solid white;
+  position: relative;
+`;
+
+const BookingOn = styled.div`
+  width: 280px;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  bottom: -323px;
+`;
---
+++ src/Component/Nav/DropDownNav.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import styled, { css, keyframes } from "styled-components";
+
+export default class DropDownNav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mouseToggle: false
+    };
+  }
+  mouseOn = () => {
+    this.state.mouseToggle
+      ? this.setState({ mouseToggle: false })
+      : this.setState({ mouseToggle: true });
+  };
+
+  render() {
+    return (
+      <NavBoxLi onMouseEnter={this.mouseOn} onMouseLeave={this.mouseOn}>
+        <DropDownTitle>Find hotel</DropDownTitle>
+        <DropDownUl hotel onOff={this.state.mouseToggle}>
+          <DropDownLi>{this.props.menu1 && this.props.menu1}</DropDownLi>
+          <DropDownLi>{this.props.menu2 && this.props.menu2}</DropDownLi>
+          <DropDownLi>{this.props.menu3 && this.props.menu3}</DropDownLi>
+          <DropDownLi>{this.props.menu4 && this.props.menu4}</DropDownLi>
+          <DropDownLi>{this.props.menu5 && this.props.menu5}</DropDownLi>
+          <DropDownLi>{this.props.menu6 && this.props.menu6}</DropDownLi>
+        </DropDownUl>
+      </NavBoxLi>
+    );
+  }
+}
+
+// 공용CSS
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavBoxLi = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 44px;
+  margin: 0 auto;
+  position: relative;
+`;
+
+const DropDownTitle = styled(Center.withComponent("div"))`
+  width: 112px;
+  height: 44px;
+  text-align: center;
+  font-size: 14px;
+  background-color: transparent;
+  color: white;
+  &:hover {
+    color: rgb(166, 129, 100);
+  }
+`;
+const DropDownUl = styled.ul`
+  position: absolute;
+  display: flex;
+  justify-content: flex-start;
+  width: 112px;
+  top: 30px;
+  padding: 25px 0 20px 50px;
+  font-size: 14px;
+  background-color: transparent;
+  display: ${props => (props.onOff ? "block" : "none")};
+  ${props => {
+    if (props.hotel) {
+      return css`
+        animation: ${fadeOn} 0.5s linear;
+      `;
+    }
+  }}
+`;
+
+const DropDownLi = styled.li`
+  height: 30px;
+  cursor: pointer;
+`;
+
+const fadeOn = keyframes`
+from{
+  opacity:0;
+  top: 64px;
+}to{
+  opacity:1;
+  top: 30px;
+}
+`;
