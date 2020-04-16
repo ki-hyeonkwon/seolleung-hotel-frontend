0fd8d539bb7debebb07136eae48b3ad549179e1e
annkimm <zizicoo208@naver.com>
2020-04-16 23:38:51 +0900

    nav, config, login, qnalist fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://localhost:3000";
+export const SERVER_URL = "http://10.58.6.27:8008";
 // export const SMS_URL = "https://api-sens.ncloud.com/v1/sms/services/";
 // export const RESERVATION_URL = "http://52.79.185.94:8000";
 export const SMS_URL = "http://10.58.6.27:8008/users";
